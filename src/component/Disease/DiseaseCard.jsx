4f4f3fe7ca4881fd0e1436e7939f51c1dce3d089
aryndraa <aryacoder1102@gmail.com>
2025-03-11 03:46:54 +0800

    disease searched

--- src/component/Disease/DiseaseCard.jsx
+++ src/component/Disease/DiseaseCard.jsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom";
 
 export const DiseaseCard = () => {
   return (
-    <Link to="1" className="p-4 lg:p-6 2xl:p-8 bg-white h-fit rounded-xl lg:rounded-xxl hover:text-white hover:bg-secondary transition ease-in-out">
+    <Link to="/disease/1" className="p-4 lg:p-6 2xl:p-8 bg-white h-fit rounded-xl lg:rounded-xxl hover:text-white hover:bg-secondary transition ease-in-out">
       <h2 className="text-xl mb-4 lg:text-2xl font-semibold lg:mb-6 2xl:mb-12">Asthma Bronchiale</h2>
       <div>
         <h3 className=" 2xl:text-lg mb-1">
--- src/component/Disease/TabBar.jsx
+++ src/component/Disease/TabBar.jsx
@@ -1,6 +1,6 @@
 export const TabBar = ({ activeTab = "overview", setActiveTab }) => {
   return (
-    <div className="bg-white grid grid-cols-3 w-fit rounded-xl lg:rounded-xxl font-medium ">
+    <div className="bg-white grid grid-cols-3 w-fit lg:w-full rounded-xl lg:rounded-xxl font-medium ">
       <button
         className={`lg:text-lg 2xl:text-xl px-3 py-3 lg:py-5 2xl:py-6 rounded-xl lg:rounded-xxl transition ease-in-out ${activeTab === "overview" ? "bg-trinary" : "bg-transparent"}`}
         onClick={() => setActiveTab("overview")}
