74e391c1c332e198c0dfdc2d549c6c4566407d3d
aryndraa <aryacoder1102@gmail.com>
2025-04-06 17:06:16 +0800

    deases data

--- src/pages/Disease/Diseases.jsx
+++ src/pages/Disease/Diseases.jsx
@@ -11,6 +11,7 @@ export function Diseases() {
       <section className="min-h-screen mt-[100px] lg:mt-[160px] mb-4">
         <div className="w-full flex  gap-2 items-center mb-4">
           <SearchBar/>
+          <ShortBy/>
         </div>
 
         <Banner
