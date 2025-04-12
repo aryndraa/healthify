de9c9c8b5f4dc3c3470715def3b9bc204ddc97f8
aryndraa <aryacoder1102@gmail.com>
2025-04-12 22:54:27 +0800

    check 11

--- src/component/Disease/ListWords.jsx
+++ src/component/Disease/ListWords.jsx
@@ -50,7 +50,7 @@
       </button>
 
       {isModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+        <div className="fixed z-50 inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className="bg-white p-6 rounded-lg shadow-lg w-[90%] max-w-md">
             <h2 className="text-lg font-bold mb-4">Select a Letter</h2>
             <div className="grid grid-cols-6 gap-2">
