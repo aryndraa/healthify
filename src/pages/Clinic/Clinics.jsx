da12c95d12c5f5f31f2f2f14844024bf39895e87
aryndraa <aryacoder1102@gmail.com>
2025-04-13 20:34:55 +0800

    banner padding change

--- src/pages/Clinic/Clinics.jsx
+++ src/pages/Clinic/Clinics.jsx
@@ -159,10 +159,10 @@ export const Banner = ({image, title, text}) => {
 		>
 			<div className="hidden lg:block bg-transparent">
 				<img src={image} alt=""
-						 className="w-full lg:w-[28rem] 2xl:w-full object-cover lg:rounded-[60px] bg rounded-lg h-full"/>
+						 className="w-full lg:w-[28rem] 2xl:h-[400px] 2xl:w-full object-cover lg:rounded-[60px] bg rounded-lg h-full"/>
 			</div>
-			<div className="bg-white flex flex-col justify-between flex-1 min-h-full rounded-xl lg:rounded-xxl p-6 lg:p-10 2xl:p-16">
-				<h1 className="font-semibold text-xl lg:text-4xl 2xl:text-5xl leading-8">
+			<div className="bg-white flex flex-col justify-between flex-1 min-h-full rounded-xl lg:rounded-xxl p-6 lg:py-12 lg:p-14 2xl:p-14">
+				<h1 className="font-semibold  text-xl lg:text-4xl 2xl:text-5xl leading-8">
 					{title}
 				</h1>
 				<p className="text-sm 2xl:text-xl">
